6a809b798754269d80fa1aa67e517a6040a0da2b
jannisB <jannisbosch@hotmail.com>
2022-08-10 16:20:53 +0200

    add make_report
    xx

---
+++ R/make_report.R
@@ -0,0 +1,18 @@
+#' Make report
+#'
+#' Creates and renders a Markdown Document with overview information about items and scales
+#'
+#' @param data A data frame with dic information
+#' @param items A logical expression for any dic attribute (e.g. scale == "ITRF" & subscale == "Int")
+#' @param scales If TRUE, variable names are returned instead of a data frame
+#' @export
+
+make_report <- function(data, items = NULL, scales = NULL) {
+  rmarkdown::render(
+    input = paste0(system.file(package = "scaledic"), "/rmd/Template.Rmd"),
+    output_file = template.pdf,
+    params = list(data = data, x1 = items, x2 = scales),
+    encoding     = 'UTF-8'
+  )
+}
+
